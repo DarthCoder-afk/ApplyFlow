e6c7bc30e3a2e0a322f43d99fa785f139cb9ca4f
DarthCoder-afk <seanmichaelborje179@gmail.com>
2026-06-14 21:22:23 +0800

    Enhance server setup with graceful shutdown and Prisma disconnection on termination signals

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,21 @@
+import "dotenv/config";
 import app from "./app";
-import dotenv from "dotenv";
 import { env } from "./config/env";
-
-dotenv.config();
+import {prisma} from "./config/database"
 
 const port = env.port;
 
-app.listen(port, () => {
+const server =app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+async function shutdown() {
+    console.log("Shutting down...");
+    server.close();
+    await prisma.$disconnect();
+    process.exit(0);
+}
+
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
